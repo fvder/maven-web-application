fa6f25960737ecd4f3f7cb891a6b15064bf7f2b1
Joe D <151402617+fvder@users.noreply.github.com>
2024-04-16 22:48:16 -0400

    Update snyk.sh

--- snyk.sh
+++ snyk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+snyk auth ${env.SNYK_TOKEN}
 # Run synk container test command
-synk container test
+synk container test fvder/tesla
 
 # Check the exit status of the command
 if [ $? -ne 0 ]; then
