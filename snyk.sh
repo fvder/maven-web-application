d9c5a8b4394d719d504774957da72c68932788eb
Joe D <151402617+fvder@users.noreply.github.com>
2024-04-16 23:22:47 -0400

    Delete snyk.sh

--- snyk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-snyk auth ${env.SNYK_TOKEN}
-# Run synk container test command
-synk container test fvder/tesla
-
-# Check the exit status of the command
-if [ $? -ne 0 ]; then
-    echo "Synk container test failed, but continuing the pipeline."
-fi
